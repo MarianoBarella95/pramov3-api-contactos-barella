e5d8560c7fde6a8ddd5b76a014c25628ff366210
MarianoBarella95 <marianobarella1@gmail.com>
2026-05-11 12:15:42 -0300

    eliminé la connetion string

--- Models/DbA358b2Pam3Context.cs
+++ Models/DbA358b2Pam3Context.cs
@@ -23,10 +23,6 @@ public partial class DbA358b2Pam3Context : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
-//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//         => optionsBuilder.UseSqlServer("Server=SQL8020.site4now.net;Database=db_a358b2_pam3;User=db_a358b2_pam3_admin;Password=tudai123;TrustServerCertificate=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Categorium>(entity =>
